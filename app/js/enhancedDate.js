9ed81fac7998dd6c50e29d0dd4ba5cda13a21575
Karen Nelson <karen.mc.nelson@gmail.com>
2015-04-17 12:31:35 -0700

    enhanced date assigment as of Friday 4-17

--- app/js/enhancedDate.js
+++ app/js/enhancedDate.js
@@ -1,5 +1,107 @@
-/* 
- * add your module definition for enhancedDate here
- * assign the module to a global variable
- * /
+var enhancedDate = (function iffe() {
+  'use strict';
 
+  //variables for all of the functions below declared here
+
+
+  var myDate;
+  var myDayName;
+  var dayNames = ["Sunday", "Monday","Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  var myMonth;
+  var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  var todaysDate;
+
+
+  return {
+
+
+    setDate: function setDate(enterDate) {
+        if (enterDate instanceof Date){
+          myDate = enterDate;
+        } else if (isNaN(enterDate) === false){
+          myDate = enterDate;
+        } else{
+          myDate = new Date();
+        }
+    },
+
+
+    getDate: function getDate(enterDate) {
+         if (enterDate instanceof Date){
+            myDate = enterDate;
+         }else if (typeof enterDate === "number"){
+            myDate = enterDate;
+         }else{
+           myDate = new Date();
+         }
+         return myDate;
+
+    },
+
+    getDayName: function getDayName(enterDate) {
+
+      if (enterDate instanceof Date){
+         myDate = enterDate;
+      }else if (typeof enterDate === "number"){
+         myDate = enterDate;
+      }else{
+        myDate = new Date();
+      }
+      myDayName = dayNames[myDate.getDay()];
+      return myDayName;
+    },
+
+    getMonthName: function getMonthName(enterDate) {
+      if (enterDate instanceof Date){
+         myDate = enterDate;
+      }else if (typeof enterDate === "number"){
+         myDate = enterDate;
+      }else{
+        myDate = new Date();
+      }
+      myMonth = monthNames[myDate.getMonth()];
+      return myMonth;
+    },
+
+    isFuture: function isFuture(enterDate) {
+      if (enterDate instanceof Date){
+         myDate = enterDate;
+      }else if (typeof enterDate === "number"){
+         myDate = enterDate;
+      }else{
+        myDate = new Date();
+      }
+
+      todaysDate= new Date();
+      if (todaysDate > myDate) {
+        return true;
+      }
+    },
+
+    isToday: function isToday(enterDate) {
+      if (enterDate instanceof Date){
+         myDate = enterDate;
+      }else if (typeof enterDate === "number"){
+         myDate = enterDate;
+      }else{
+        myDate = new Date();
+      }
+
+      todaysDate= new Date();
+      if (myDate === todaysDate) {
+        return true;
+      }
+    }
+  };
+})();
+
+enhancedDate.getDayName();
+enhancedDate.getMonthName();
+
+var message = '';
+
+enhancedDate.setDate(new Date());
+
+message += 'Today is ' + enhancedDate.getDayName() + ' in the month of ' + enhancedDate.getMonthName();
+
+console.log(message);
